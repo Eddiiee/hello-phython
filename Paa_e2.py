a43bd4521e396d68123620e7f3933b9217601078
Edd <eddiiee2001@gmail.com>
2018-10-27 01:10:39 +0800

    Act2

--- Paa_e2.py
+++ Paa_e2.py
@@ -1,26 +1,23 @@
 import sys
 
-
-
 Wind = sys.argv[1]
 Wind = int(sys.argv[1])
 
 
-if Wind >220: 
-     print ("Category", "Super Typhoon")  
+if  Wind >=220: 
+     print ("Category:", "|Super Typhoon|") 
 
-elif Wind == 118 or Wind <= 220:
-     print ("Category", "Typhoon")   
+elif Wind >= 118 or Wind == 220:
+     print ("Category:", "|Typhoon|")  
 
-elif Wind == 89 or Wind <= 117:
-     print ("Category", "Severe Tropical Storm")  
+elif Wind >= 89 or Wind >= 117:
+     print ("Category:", "|Severe Tropical Storm|") 
 
+elif Wind >= 62 or Wind >= 88:
+     print ("Category:", "|Tropical Storm|")  
 
-elif Wind == 62 or Wind <= 88:
-    print ("Category", "Tropical Storm"     
-
-elif Wind >= 61:
-    print ("Tropical Depression")
+elif Wind == 61:
+     print ("Category:", "|Tropical Depression|")
 
 
 
